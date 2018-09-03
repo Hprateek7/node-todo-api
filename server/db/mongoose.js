d52b2d7d784e277d04aecd85364d35cb07b5b455
Harsh Prateek <hprateek1@gmail.com>
2018-09-03 15:17:21 +0530

    Deploy to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/Todo');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/Todo');
 
 module.exports = {
     mongoose
